1267c8df9320a114d76aa00bec965409ca8c707c
Kristian Sakarisson <kristian@sakarisson.com>
2018-04-13 20:23:14 +0300

    Added empty CalendarView class

---
+++ app/src/main/java/com/sakarisson/kristian/androidassignment3/CalendarView.java
@@ -0,0 +1,11 @@
+package com.sakarisson.kristian.androidassignment3;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public final class CalendarView extends View {
+    public CalendarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
