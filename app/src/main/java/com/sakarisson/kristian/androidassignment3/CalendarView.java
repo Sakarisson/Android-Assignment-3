5e71d1d56c50c34def670282a1401442fce1652a
Kristian Sakarisson <kristian@sakarisson.com>
2018-04-14 21:13:37 +0300

    Added onDraw override

--- app/src/main/java/com/sakarisson/kristian/androidassignment3/CalendarView.java
+++ app/src/main/java/com/sakarisson/kristian/androidassignment3/CalendarView.java
@@ -4,13 +4,20 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageView;
 
 public final class CalendarView extends View {
     private Bitmap background;
+    private ImageView mImageView;
+    private Context mContext;
+
     public CalendarView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mContext = context;
         TypedArray a = context.getTheme().obtainStyledAttributes(
                 attrs,
                 R.styleable.CalendarView,
@@ -21,5 +28,12 @@ public final class CalendarView extends View {
         } finally {
             a.recycle();
         }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawColor(Color.LTGRAY);
+        canvas.drawBitmap(background, 0, 0, null);
     }
 }
