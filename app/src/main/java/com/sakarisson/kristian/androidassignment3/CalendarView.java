659405da0a8a1fbec302923d4a9906ae22615321
Kristian Sakarisson <kristian@sakarisson.com>
2018-04-15 21:52:03 +0300

    Refactoring

--- app/src/main/java/com/sakarisson/kristian/androidassignment3/CalendarView.java
+++ app/src/main/java/com/sakarisson/kristian/androidassignment3/CalendarView.java
@@ -6,19 +6,23 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.ImageView;
+
+import java.util.Calendar;
 
 public final class CalendarView extends View {
     private Bitmap background;
-    private Bitmap scaledBackground;
-    private ImageView mImageView;
-    private Context mContext;
+    private Calendar mCalendar;
 
     public CalendarView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mContext = context;
+        mCalendar = Calendar.getInstance();
+        mCalendar.add(Calendar.DAY_OF_MONTH, 1);
         TypedArray a = context.getTheme().obtainStyledAttributes(
                 attrs,
                 R.styleable.CalendarView,
@@ -31,17 +35,29 @@ public final class CalendarView extends View {
         }
     }
 
+    private Bitmap generateScaledImage(int width, int height) {
+        Bitmap scaledBackground = Bitmap.createScaledBitmap(background, width, height, true);
+        return scaledBackground;
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        scaledBackground = Bitmap.createScaledBitmap(background, canvas.getWidth(), canvas.getHeight(), true);
         canvas.drawColor(Color.LTGRAY);
-        canvas.drawBitmap(scaledBackground, 0, 0, null);
+        canvas.drawBitmap(generateScaledImage(canvas.getWidth(), canvas.getHeight()), 0, 0, null);
+        // Get text dimensions
+        TextPaint textPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG | Paint.LINEAR_TEXT_FLAG);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setColor(Color.parseColor("#ff00ff"));
+        textPaint.setTextSize(30);
+
+        StaticLayout mTextLayout = new StaticLayout("hello", textPaint, 100, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
+        mTextLayout.draw(canvas);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(widthMeasureSpec, widthMeasureSpec);
     }
 }
